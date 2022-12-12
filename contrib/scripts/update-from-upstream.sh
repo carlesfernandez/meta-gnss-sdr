f2e0592cf23f26e0ae1d6788621649473c8f73da
Carles Fernandez <carles.fernandez@gmail.com>
2022-12-12 12:50:15 +0100

    Improve scripts

--- contrib/scripts/update-from-upstream.sh
+++ contrib/scripts/update-from-upstream.sh
@@ -18,7 +18,9 @@ display_usage() {
     echo "update-from-upstream.sh v$version - This script pulls all branches from the upstream repo."
     echo " Supported branches: $branches"   
     echo " Usage:"
-    echo "  ./update-from-upstream.sh"
+    echo "  ./update-from-upstream.sh remote"
+    echo "  where:"
+    echo "   remote   [optional] upstream remote name (or full URL)"
 }
 
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
@@ -31,6 +33,12 @@ if [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
     exit 0
 fi
 
+if [ "$#" -gt 1 ]; then
+   echo "Error: wrong number of parameters."
+   display_usage
+   exit 1
+fi
+
 upstream="https://github.com/carlesfernandez/meta-gnss-sdr"
 currentbranch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
@@ -41,11 +49,9 @@ fi
 
 echo "Running update-from-upstream.sh v$version ..."
 
-git fetch $upstream
-
 for branch in $branches; do
     git checkout "$branch"
-    git pull $upstream "$branch"
+    git pull "$upstream" "$branch"
 done
 
 git checkout "$currentbranch"
