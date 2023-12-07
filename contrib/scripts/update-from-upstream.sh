ed1f3214ce51c188866ecee754a37e403d4bf344
Carles Fernandez <carles.fernandez@gmail.com>
2023-12-07 11:26:13 +0100

    Add nanbield to scripts

--- contrib/scripts/update-from-upstream.sh
+++ contrib/scripts/update-from-upstream.sh
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: MIT
 
 version="1.0"
-branches="master mickledore langdale kirkstone honister hardknott gatesgarth dunfell zeus warrior thud sumo rocko"
+branches="master nanbield mickledore langdale kirkstone honister hardknott gatesgarth dunfell zeus warrior thud sumo rocko"
 
 if ! [ -x "$(command -v git)" ]; then
     echo "Please install git before using this script."
